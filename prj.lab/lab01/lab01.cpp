0132952ed3d55cef93c59e2ae03ae02d5666cd90
user <kygrachev@hse.ru>
2024-02-25 14:09:28 +0300

    Установил opencv

--- prj.lab/lab01/lab01.cpp
+++ prj.lab/lab01/lab01.cpp
@@ -1,10 +1,9 @@
+#include <iostream>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 
 
 int main() {
-  cv::Mat1b m(200, 200, 127);
-  cv::imshow("example", m);
-  cv::waitKey(0);
+  std::cout << CV_VERSION << std::endl;
 }
